a29a4effe05e9ac3721c62288d898700f97cf76a
thehabdash <brent@thehabdash.com>
2016-04-09 15:39:59 -0400

    first commit

---
+++ scripts/tabletop-feed.js
@@ -0,0 +1,66 @@
+var jqueryNoConflict = jQuery;
+
+// begin main function
+jqueryNoConflict(document).ready(function(){
+
+    initializeTabletopObject("https://docs.google.com/spreadsheets/d/1sFC465ZOk_VtfkxsZySlSaYPm7nEvkgXf50fVJ3sAnM/pubhtml");
+
+});
+
+// pull data from google spreadsheet
+function initializeTabletopObject(dataSpreadsheet){
+    Tabletop.init({
+        key: dataSpreadsheet,
+        callback: writeTableWith,
+        simpleSheet: true,
+        debug: false
+    });
+}
+
+// create table headers
+function createTableColumns(){
+
+    /* swap out the properties of mDataProp & sTitle to reflect
+    the names of columns or keys you want to display.
+    Remember, tabletop.js strips out spaces from column titles, which
+    is what happens with the More Info column header */
+
+    var tableColumns =   [
+		{"mDataProp": "title", "sTitle": "Project Title", "sClass": "center"},
+		//{"mDataProp": "url", "sTitle": "URL", "sClass": "center"},
+		{"mDataProp": "description", "sTitle": "Description", "sClass": "center"},
+		//{"mDataProp": "tags", "sTitle": "Tags", "sClass": "center"},
+	];
+    return tableColumns;
+}
+
+// create the table container and object
+function writeTableWith(dataSource){
+
+    jqueryNoConflict("#demo").html("<table cellpadding='0' cellspacing='0' border='0' class='display table table-bordered table-striped' id='data-table-container'></table>");
+
+    var oTable = jqueryNoConflict("#data-table-container").dataTable({
+        "sPaginationType": "bootstrap",
+        "iDisplayLength": 25,
+        "aaData": dataSource,
+        "aoColumns": createTableColumns(),
+        "fnRowCallback": function(nRow, aData, iDisplayIndex) {
+            console.log(aData);
+            $("td:eq(0)", nRow).html("<a href='http://" + aData.url + "'>"+ aData.title +"</a>");
+            return nRow;
+        },
+        "oLanguage": {
+            "sLengthMenu": "_MENU_ records per page"
+        }
+    });
+
+};
+
+//define two custom functions (asc and desc) for string sorting
+jQuery.fn.dataTableExt.oSort["string-case-asc"]  = function(x,y) {
+	return ((x < y) ? -1 : ((x > y) ?  0 : 0));
+};
+
+jQuery.fn.dataTableExt.oSort["string-case-desc"] = function(x,y) {
+	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+};
